4d80e4e68696663fe152202170eb3ed029c39993
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-05-01 23:50:00 -0400

    Update BinaryTree.py

--- BinaryTree.py
+++ BinaryTree.py
@@ -7,6 +7,8 @@ class Node(self, data):
         self.right = None
     
     #function to insert item into binary tree.
+    #building the function so if the item is less than the node it goes to the left, 
+    # greater than goes to the right
     def insert(self, data):
         
 
