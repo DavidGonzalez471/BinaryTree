d92f739b4144db58a0599784806a50a663568259
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-05-01 23:49:18 -0400

    Update BinaryTree.py

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,8 +1,13 @@
 #Binary Tree Implementation
 
 class Node(self, data):
-    self.data = data
-    self.left = None
-    self.right = None
     
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    #function to insert item into binary tree.
+    def insert(self, data):
+        
 
+    
